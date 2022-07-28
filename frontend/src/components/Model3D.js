876c4b06858431752c9d9e7c70736b23cc905d7e
AyushmanRocks <ayushmanpandey2312@gmail.com>
2022-07-28 19:01:57 +0530

    .

--- frontend/src/components/Model3D.js
+++ frontend/src/components/Model3D.js
@@ -26,9 +26,11 @@ function Model({ ...props }) {
   const showModel = () => {
     return Object.keys(nodes).filter(node => (
       nodes[node].type === "Mesh"
-    )).map(obj => {
-      
-      return <mesh geometry={nodes[obj].geometry} material={materials.laces} material-color={'#f00'}/>
+    )).map((obj, i) => {
+      let cur_mat = materials[Object.keys(materials)[i]];
+      // console.log(cur_mat.name);
+      // console.log(props);
+      return <mesh geometry={nodes[obj].geometry} material={cur_mat} material-color={props.customColors[cur_mat.name]}/>
     })
   }
   
@@ -45,12 +47,18 @@ function Model3D() {
  
   const [mesh,setMesh] = useState("#ffffff")
   const [stripes,setStripes] = useState("#ffffff")
-  const [soul,setSoul] = useState("#ffffff")
+  const [sole, setSole] = useState("#ffffff");
+  const [laces, setLaces] = useState("#ffffff");
+  const [band, setBand] = useState("#ffffff");
+  const [patch, setPatch] = useState("#ffffff");
+  const [caps, setCaps] = useState("#ffffff");
+  const [inner, setInner] = useState("#ffffff");
+
 
   return (
     <div className="App">
         <div className="wrapper">
-            {/* <div className="card"> */}
+            
                 <div className="product-canvas" style={{marginLeft:'10px'}}>
                    <Canvas>
                       <Suspense fallback={null}>
@@ -60,7 +68,7 @@ function Model3D() {
                                      penumbra={1} 
                                      position={[10,15,10]}
                                      castShadow />
-                          <Model customColors={{mesh:mesh, stripes:stripes , soul:soul }}/>
+                          <Model customColors={{mesh:mesh, stripes:stripes , sole:sole, laces:laces, band:band, patch:patch, caps:caps, inner:inner}}/>
                           <OrbitControls enablePan={true}
                                          enableZoom={true}
                                          enableRotate={true}/>
@@ -71,30 +79,61 @@ function Model3D() {
                  
 
 
-                <div className='colors' >
-                    <div>
+                <div className='colors ' style={{display:"block"}}>
+                    <div className='card' >
+                      <div>
                         <input type="color" id="mesh" name="mesh"
                               value={mesh} 
                               onChange={(e) => setMesh(e.target.value)}/>
-                        <label for="mesh">Main</label>
+                        <h4 style={{display:"inline"}} for="mesh">Main</h4>
                       </div>
 
                     <div>
                         <input type="color" id="stripes" name="stripes"
                                 value= {stripes}
                                 onChange={(e) => setStripes(e.target.value)}/>
-                        <label for="stripes">Stripes</label>
+                        <h4 style={{display:"inline"}} for="stripes">Stripes</h4>
                     </div>
                     <div>
                         <input type="color" id="soul" name="soul"
-                                value={soul} 
-                                onChange={(e) => setSoul(e.target.value)}/>
-                        <label for="soul">Soul</label>
+                                value={sole} 
+                                onChange={(e) => setSole(e.target.value)}/>
+                        <h4 style={{display:"inline"}} for="soul">Sole</h4>
+                    </div>
+                    <div>
+                        <input type="color" id="laces" name="laces"
+                                value={laces} 
+                                onChange={(e) => setLaces(e.target.value)}/>
+                        <h4 style={{display:"inline"}}for="laces">Laces</h4 >
+                    </div>
+                    <div>
+                        <input type="color" id="band" name="band"
+                                value={band} 
+                                onChange={(e) => setBand(e.target.value)}/>
+                        <h4 style={{display:"inline"}}for="band">Band</h4 >
+                    </div>
+                    <div>
+                        <input type="color" id="patch" name="patch"
+                                value={patch} 
+                                onChange={(e) => setPatch(e.target.value)}/>
+                        <h4 style={{display:"inline"}}for="patch">Patch</h4>
+                    </div>
+                    <div>
+                        <input type="color" id="caps" name="caps"
+                                value={caps} 
+                                onChange={(e) => setCaps(e.target.value)}/>
+                        <h4 style={{display:"inline"}} for="caps">Caps</h4>
+                    </div>
+                    <div>
+                        <input type="color" id="inner" name="inner"
+                                value={inner} 
+                                onChange={(e) => setInner(e.target.value)}/>
+                        <h4 style={{display:"inline"}}for="inner">Inner</h4>
                     </div>
                 </div>
             </div>
         </div>
-    // </div>
+     </div>
   );
 };
 
